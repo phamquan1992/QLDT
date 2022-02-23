bd97a90397728d384868b2557fe2b82e19240018
quan <quanpq@teca.vn>
2022-02-23 15:30:13 +0700

    commit lần 3

--- QLDT.Core/Domain/DM_CHUCVU.cs
+++ QLDT.Core/Domain/DM_CHUCVU.cs
@@ -9,8 +9,8 @@ namespace QLDT.Core.Domain
         public virtual string ma_chuc_vu { get; set; }
         public virtual string ten_chuc_vu { get; set; }
         public virtual string mo_ta { get; set; }
-        public virtual DateTime creation_date { get; set; }
-        public virtual DateTime last_update_date { get; set; }
+        public virtual DateTime? creation_date { get; set; }
+        public virtual DateTime? last_update_date { get; set; }
         public virtual string created_by { get; set; }
         public virtual string last_update_by { get; set; }
     }
