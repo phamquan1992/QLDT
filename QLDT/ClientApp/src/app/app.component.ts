1f04943037e000bbc0068963ebb4e5de378450ea
quan <quanpq@teca.vn>
2022-03-05 09:59:30 +0700

    85

--- QLDT/ClientApp/src/app/app.component.ts
+++ QLDT/ClientApp/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ClientApp';
+  istoogle: boolean = false;
+  showToogle() {
+    this.istoogle = !this.istoogle;
+  }
 }
